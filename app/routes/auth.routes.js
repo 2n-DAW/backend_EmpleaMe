41483f9d58855fe660f976e739d48c74a0432bd2
Llorens19 <llorenssorianodiego@gmail.com>
2024-10-29 12:55:47 +0100

    Cambiar rutas de acceso de los endPoints

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,11 +3,11 @@ const verifyJWT = require('../middleware/verifyJWT.js');
 module.exports = (app) => {
     const { userType, clientUserLogin, registerUser, getCurrentUser, updateUser, logout } = require('../controllers/auth.controller.js');
 
-    app.post('/users/userType', userType);
+    app.post('/user/type', userType);
     
-    app.post('/users/login', clientUserLogin);
+    app.post('/user/login', clientUserLogin);
 
-    app.post('/users/register', registerUser);
+    app.post('/user', registerUser);
 
     app.get('/user', verifyJWT, getCurrentUser);
 
